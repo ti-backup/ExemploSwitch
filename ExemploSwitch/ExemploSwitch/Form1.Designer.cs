6be55c0ce74c57ef0064f81d894dddc36f79ba4e
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-06-04 19:53:04 -0300

    exemplo completo

---
+++ ExemploSwitch/ExemploSwitch/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ExemploSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbVeiculos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVeiculos = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbVeiculos
+            // 
+            this.cmbVeiculos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVeiculos.FormattingEnabled = true;
+            this.cmbVeiculos.Location = new System.Drawing.Point(70, 48);
+            this.cmbVeiculos.Name = "cmbVeiculos";
+            this.cmbVeiculos.Size = new System.Drawing.Size(84, 21);
+            this.cmbVeiculos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Veículos:";
+            // 
+            // btnVeiculos
+            // 
+            this.btnVeiculos.Location = new System.Drawing.Point(13, 13);
+            this.btnVeiculos.Name = "btnVeiculos";
+            this.btnVeiculos.Size = new System.Drawing.Size(222, 23);
+            this.btnVeiculos.TabIndex = 2;
+            this.btnVeiculos.Text = "Gera Veículos";
+            this.btnVeiculos.UseVisualStyleBackColor = true;
+            this.btnVeiculos.Click += new System.EventHandler(this.btnVeiculos_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(160, 46);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 96);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnVeiculos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbVeiculos);
+            this.Name = "Form1";
+            this.Text = "Exemplo Switch Case";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbVeiculos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVeiculos;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ ExemploSwitch/ExemploSwitch/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploSwitch
+{
+    public partial class Form1 : Form
+    {
+        List<String> veiculos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            veiculos = new List<String>();
+        }
+
+        private void btnVeiculos_Click(object sender, EventArgs e)
+        {
+            veiculos.Add("Moto");
+            veiculos.Add("Carro");
+            veiculos.Add("Bicicleta");
+            veiculos.Add("Navio");
+
+            PreencheComboVeiculos();
+        }
+
+        private void PreencheComboVeiculos()
+        {
+            for (Byte i = 0; i < veiculos.Count; i++)
+            {
+                cmbVeiculos.Items.Add(veiculos.ElementAt(i));
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            String opcao = cmbVeiculos.Text;
+            switch (opcao)
+            {
+                case "Moto":
+                    MessageBox.Show("mooooooto!!!");
+                    break;
+                case "Carro":
+                    MessageBox.Show("caaaaarro!!!");
+                    break;
+                case "Bicicleta":
+                    MessageBox.Show("biiiiiike!!!");
+                    break;
+                case "Navio":
+                    MessageBox.Show("naviiiiio!!!");
+                    break;
+                default:
+                    MessageBox.Show("Selecione uma opção");
+                    break;
+            }
+        }
+    }
+}
